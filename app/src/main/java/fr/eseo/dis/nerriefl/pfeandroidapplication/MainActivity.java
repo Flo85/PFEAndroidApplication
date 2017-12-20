9a5e50ee05c69eec9b70510e15abcb055314b08a
Flo85 <florian.nerriere@gmail.com>
2017-12-20 09:06:00 +0100

    Creating the project

---
+++ app/src/main/java/fr/eseo/dis/nerriefl/pfeandroidapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.eseo.dis.nerriefl.pfeandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
